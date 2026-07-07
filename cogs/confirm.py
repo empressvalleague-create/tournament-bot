13cdf4453bc23ad1e8bfb763b66ae2a9f03e9367
empressvalleague-create <empressvalleague@gmail.com>
2026-07-07 17:30:49 -0500

    confirm: start time options at 9:00 AM instead of 12:00 PM

--- cogs/confirm.py
+++ cogs/confirm.py
@@ -45,6 +45,7 @@ def get_next_14_dates():
     return [(today + timedelta(days=i)).strftime("%A, %b %d") for i in range(14)]
 
 TIMES = [
+    "9:00 AM","9:30 AM","10:00 AM","10:30 AM","11:00 AM","11:30 AM",
     "12:00 PM","12:30 PM",
     "1:00 PM","1:30 PM","2:00 PM","2:30 PM","3:00 PM","3:30 PM",
     "4:00 PM","4:30 PM","5:00 PM","5:30 PM","6:00 PM","6:30 PM",
